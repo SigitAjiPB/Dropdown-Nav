3c2f4967d36d4f84efe22ed576bb3f65715a6932
Sigit Aji P <sigitnj89@gmail.com>
2023-09-17 22:26:32 +0700

    add a function tht use to add list Todo to the main container

--- public/script.js
+++ public/script.js
@@ -13,3 +13,51 @@ dropdownBtn.addEventListener('click', () => {
     dropdownMenu.classList.toggle('hidden');
 });
 
+const addBtn = document.getElementById('addBtn');
+const listInput = document.getElementById('listInput');
+const listTodo = document.getElementById('listTodo');
+const mainContainer = document.getElementById('mainContainer');
+const inputForm = document.getElementById('inputForm');
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+addBtn.addEventListener('click', (e) => {
+    if (listInput.value != "") {
+        displayTodo(listInput.value);
+        listInput.value = "";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+displayTodo = () => {
+    let listWrapper = document.createElement("section");
+    listWrapper.classList.add("listWrapper");
+
+    listWrapper.innerHTML = `<div id="listTodo" class="flex justify-between items-center gap-4 mb-3">
+    <div class="bg-slate-200 flex w-full rounded-lg p-2 gap-2 items-center justify-between">
+        <button type="button" class="bg-slate-300 flex justify-center items-center rounded-lg text-slate-300 focus:text-white focus:bg-slate-500"><span class="material-symbols-outlined">done</span></button>
+            
+        <p class="max-w-none w-full">${listInput.value}</p>
+    </div>
+    <button id="deleteBtn" class="bg-red-400 text-white flex items-center justify-center rounded-lg h-8 w-10 hover:bg-red-500"><span class="material-symbols-outlined">delete</span></button>
+    </div>`;
+
+    let deleteBtn = listTodo.querySelector('#deleteBtn');
+
+    deleteBtn.addEventListener('click', () => {
+        listWrapper.remove();
+    });
+
+
+    mainContainer.appendChild(listWrapper);
+}
